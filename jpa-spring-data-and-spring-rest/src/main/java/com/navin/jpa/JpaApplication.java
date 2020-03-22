162e4b36041ca2ab22e3c2a0c73310cad2a3fcdc
patilnavin001 <patilnavin001@gmail.com>
2020-03-22 13:57:43 +0530

    JPA Demo

--- jpa-spring-data-and-spring-rest/src/main/java/com/navin/jpa/JpaApplication.java
+++ jpa-spring-data-and-spring-rest/src/main/java/com/navin/jpa/JpaApplication.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.JpaSort;
 
 import java.util.Date;
+import java.util.List;
 
 @SpringBootApplication
 public class JpaApplication implements CommandLineRunner {
@@ -31,7 +32,7 @@ public class JpaApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args){
 
-		System.out.println("############################JPA Repository##########################");
+		System.out.println("############################ JPA Repository ##########################");
 		System.out.println("\n####### Inserting Person  #######");
 		personRepository.savePerson(new Person("JAMES","NEWYORK", new Date()));
 		personRepository.savePerson(new Person("Nancy","Denmark", new Date()));
@@ -50,10 +51,11 @@ public class JpaApplication implements CommandLineRunner {
 		System.out.println(personRepository.findAll());
 
 
-		System.out.println("\n\n############################SPRING DATA JPA Repository##########################");
+		System.out.println("\n\n############################ SPRING DATA JPA Repository ##########################");
 
 		System.out.println("\n####### Inserting Person  #######");
 		personSpringDataRepository.save(new Person("LOGAN","Denmark", new Date()));
+		personSpringDataRepository.save(new Person("Paul100","Los Angeles", new Date()));
 
 		System.out.println("\n####### getPersonbyId 4  #######");
 		System.out.println(personSpringDataRepository.findById(4L));
@@ -61,18 +63,22 @@ public class JpaApplication implements CommandLineRunner {
 		System.out.println("\n####### Person All #######");
 		System.out.println(personSpringDataRepository.findAll());
 
-		System.out.println("\n####### Person All applying sorting#######");
+		System.out.println("\n####### Person All applying sorting #######");
 		System.out.println(personSpringDataRepository.findAll(Sort.by(Sort.Direction.DESC,"name")));
 
-		System.out.println("\n####### Person All applying Paginaton#######");
-		System.out.println("\n####### Person Page first#######");
+		System.out.println("\n####### Person All applying Paginaton #######");
+		System.out.println("\n####### Person Page first #######");
 		PageRequest pageRequest = PageRequest.of(0,2);
 		Page<Person> firstPage = personSpringDataRepository.findAll(pageRequest);
 		System.out.println(firstPage.getContent());
 
-		System.out.println("\n####### Person Page Second#######");
+		System.out.println("\n####### Person Page Second #######");
 		Pageable secondRequest = firstPage.nextPageable();
 		Page<Person> secondPage = personSpringDataRepository.findAll(secondRequest);
 		System.out.println(secondPage.getContent());
+
+		System.out.println("\n####### Persons whose name like 100 #######");
+		List<Person> personList = personSpringDataRepository.personWithNameLike100();
+		System.out.println(personList);
 	}
 }
--- jpa-spring-data-and-spring-rest/src/main/java/com/navin/jpa/repository/PersonSpringDataRepository.java
+++ jpa-spring-data-and-spring-rest/src/main/java/com/navin/jpa/repository/PersonSpringDataRepository.java
@@ -2,6 +2,7 @@ package com.navin.jpa.repository;
 
 import com.navin.jpa.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,4 +20,13 @@ public interface PersonSpringDataRepository extends JpaRepository<Person, Long> 
 
     @Override
     <S extends Person> S save(S s);
+
+    @Query("Select p From Person p where name like '%100'")
+    List<Person> personWithNameLike100();
+
+    @Query(value = "Select * From Person p where name like '%100'", nativeQuery = true)
+    List<Person> personWithNameLike100WithNativeQuery();
+
+    @Query(name = "find_all_persons")
+    List<Person> personWithNameLike100UsingNativeQuery();
 }
